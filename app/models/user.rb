0dcc7ffe8df2f96c08d985b19bb1773dd8818c37
Kyouka0w0 <askarte0w0@gmail.com>
2020-09-07 14:38:08 +0900

    バリデーション完成（仮）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  devise :validatable, password_length: 6..12
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'passwordには英字と数字の両方を含めて設定してください'
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true

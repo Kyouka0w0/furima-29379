b464dd347d9b09003126278066be8a4e2c2c4f78
Kyouka0w0 <askarte0w0@gmail.com>
2020-09-09 12:22:17 +0900

    テストコード・FactoryBot　修復

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,11 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX
   validates :nickname, presence: true
   validates :email, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
     validates :first_name
     validates :last_name
     validates :first_name_kana
     validates :last_name_kana
-    validates :birthday, presence: true
   end
   validates :birthday, presence: true
 end

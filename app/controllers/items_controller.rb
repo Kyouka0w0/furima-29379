9a4332de98e2179c2482b1c660d143f4f2eb11a4
Kyouka0w0 <askarte0w0@gmail.com>
2020-09-10 18:05:12 +0900

    Rubocop -a　実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,33 +1,33 @@
 class ItemsController < ApplicationController
-
   def index
     @items = Item.all
   end
-  
+
   def new
     @item = Item.new
   end
+
   def create
     Item.create(item_params)
   end
 
-  #def destroy
-    #@item = Item.find(params[:id])
-    #item.destroy
-  #end
-  #def edit
-    #@item = item.find(params[:id])
-  #end
-  #def update
-    #item = Item.find(params[:id])
-    #item.update(item_params)
-  #end
-  #def show
-    #@item = Item.find(params[:id])
-  #end
+  # def destroy
+  # @item = Item.find(params[:id])
+  # item.destroy
+  # end
+  # def edit
+  # @item = item.find(params[:id])
+  # end
+  # def update
+  # item = Item.find(params[:id])
+  # item.update(item_params)
+  # end
+  # def show
+  # @item = Item.find(params[:id])
+  # end
 
   private
-  
+
   def item_params
     params.require(:item).permit(:name, :introduction, :item_condition_id, :category_id, :price, :postage_payer_id, :shipment_id, :preparation_day_id)
   end

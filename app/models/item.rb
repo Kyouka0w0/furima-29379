3e727ddd5bdee85402642aaeb95a9ae7575de19d
Kyouka0w0 <askarte0w0@gmail.com>
2020-09-14 19:58:21 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipment
   belongs_to_active_hash :preparation
 
-  validates :name, :introduction,:price, presence: true
+  validates :name, :introduction, :image, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }
   validates :category_id, :item_condition_id, :postage_payer_id, :shipment_id, :preparation_day_id, numericality: { other_than: 1 } 
 end

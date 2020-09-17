e85bb48509891c0424fc0c65533f891da38c296e
Kyouka0w0 <askarte0w0@gmail.com>
2020-09-17 14:38:02 +0900

    エラーハンドリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,13 +27,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-  @item = Item.find(params[:id])
-  if @item.valid?
-     @item.update(item_params)
-     redirect_to root_path
-  else
-    render :edit
-  end
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   # def destroy

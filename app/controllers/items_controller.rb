d89afa7fbff70592bbee2c53c7573bd19a67f5ad
Kyouka0w0 <askarte0w0@gmail.com>
2020-09-17 13:07:44 +0900

    コントローラー・ビュー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit :show]
+  before_action :set_item, only: [:edit, :show]
 
   def index
     @item = Item.order('created_at DESC')
@@ -23,15 +23,22 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+  @item = Item.find(params[:id])
+  if @item.valid?
+     @item.update(item_params)
+     redirect_to root_path
+  else
+    render :edit
+  end
+  end
+
   # def destroy
   # @item = Item.find(params[:id])
   # item.destroy
-  # end
-  # def edit
-  # end
-  # def update
-  # item = Item.find(params[:id])
-  # item.update(item_params)
   # end
 
   private

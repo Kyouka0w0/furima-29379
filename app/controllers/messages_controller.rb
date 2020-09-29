92221e3897e44e90a170537f35115d0d33573ff9
Kyouka0w0 <askarte0w0@gmail.com>
2020-09-29 15:44:40 +0900

    コメント機能実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,21 @@
 class MessagesController < ApplicationController
+
   def new
+    @message.new
+    @messages = Message.all
   end
 
   def create
+    @message = Message.new(message_params)
+    if @message.save
+      ActionCable.server.broadcast 'message_channel', content: @message
+    end
+  end
+
+  private
+  
+  def message_params
+    params.require(:message).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
   end
   
 end
